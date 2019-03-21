0e175abd6f1989fb351c5265b4e2f9c75f3a375e
Jeff Andrews <jeff@paintedbyjeff.com>
2019-03-21 16:22:05 -0400

    work 1621 032119

---
+++ coderjoke.js
@@ -0,0 +1,7 @@
+democracy.onload = function() {
+    var america =
+    document.getElementById("voter");
+    var trump =
+    document.getElementById("president");
+    america.removeChild(trump);
+}
